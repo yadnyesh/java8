88309fa1be3d11694ed7e36d145185e1b6942485
Yadnyesh Bharat Juvekar <yadnyesh.bharatjuvekar@target.com>
2017-03-22 12:09:20 +0530

    ArrayList

---
+++ src/io/yadnyesh/ArrayListDemo.java
@@ -0,0 +1,21 @@
+package io.yadnyesh;
+
+import java.util.ArrayList;
+
+public class ArrayListDemo {
+	public static void main(String[] args) {
+		ArrayList l = new ArrayList();
+		l.add("A");
+		l.add(10);
+		l.add("A");
+		l.add(null);
+		System.out.println(l);
+		l.remove(2);
+		System.out.println(l);
+		l.add(2, "M");
+		l.add("N");
+		System.out.println(l);
+		
+	}
+
+}
